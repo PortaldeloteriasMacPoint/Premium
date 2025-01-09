6c197075b05e4e979b5a0a711bdb07348335e130
PortaldeloteriasMacPoint <portaldeloteriasmacpoint@gmail.com>
2025-01-09 00:35:10 -0300

    Create AuthController.php

---
+++ src/AuthController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+use Firebase\Auth\Token\Exception\InvalidToken;
+
+class AuthController
+{
+    private $auth;
+
+    public function __construct($firebase)
+    {
+        $this->auth = $firebase->createAuth();
+    }
+
+    public function verifyIdToken($idToken)
+    {
+        try {
+            $verifiedIdToken = $this->auth->verifyIdToken($idToken);
+            return $verifiedIdToken;
+        } catch (InvalidToken $e) {
+            return null;
+        }
+    }
+}
+
+ 
