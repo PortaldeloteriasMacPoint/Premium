63d6d8245f5279464e6a26173255078b84f1bfe7
PortaldeloteriasMacPoint <portaldeloteriasmacpoint@gmail.com>
2025-01-09 00:20:38 -0300

    Update and rename style.css to src/Auth.php

---
+++ src/Auth.php
@@ -0,0 +1,52 @@
+
+
+<?php
+
+namespace App;
+
+use Firebase\Auth\Token\Exception\InvalidToken;
+use Firebase\Auth\Token\Verifier;
+use Firebase\JWT\JWT;
+use Firebase\Auth\Token\Handler;
+use Firebase\ServiceAccount;
+
+class Auth
+{
+    private $auth;
+    private $database;
+
+    public function __construct($firebase)
+    {
+        $this->auth = $firebase->createAuth();
+        $this->database = $firebase->createDatabase();
+    }
+
+    public function verifyToken($idToken)
+    {
+        try {
+            $verifiedIdToken = $this->auth->verifyIdToken($idToken);
+            return $verifiedIdToken;
+        } catch (InvalidToken $e) {
+            throw new \Exception("Invalid ID token.");
+        }
+    }
+
+    public function getUser($uid)
+    {
+        try {
+            $user = $this->auth->getUser($uid);
+            return $user;
+        } catch (\Firebase\Auth\Exception\AuthException $e) {
+            throw new \Exception("User not found.");
+        }
+    }
+
+    public function getUserFromDatabase($uid)
+    {
+        $ref = $this->database->getReference('users/' . $uid);
+        return $ref->getValue();
+    }
+}
+
+
+
