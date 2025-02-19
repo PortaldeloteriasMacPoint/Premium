49ce86789640ad9ad9a74cb3d4eb83290c583105
PortaldeloteriasMacPoint <portaldeloteriasmacpoint@gmail.com>
2025-02-19 17:37:08 -0300

    Update Config.php

--- Config.php
+++ Config.php
@@ -1,10 +1,15 @@
-
-
 <?php
-use Kreait\Firebase\Factory;
+// Caminho do arquivo de usuários (dados)
+define('USUARIOS_FILE', 'data/usuarios.json');
 
-$firebase = (new Factory)->withServiceAccount('path/to/firebase_credentials.json')->create();
-$auth = $firebase->getAuth();
+// Função para carregar os dados dos usuários
+function carregarUsuarios() {
+    return json_decode(file_get_contents(USUARIOS_FILE), true);
+}
+
+// Função para salvar os dados dos usuários
+function salvarUsuarios($usuarios) {
+    file_put_contents(USUARIOS_FILE, json_encode($usuarios, JSON_PRETTY_PRINT));
+}
 ?>
-
 
