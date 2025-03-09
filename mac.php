17b100fe35d4957b6eb6e601a72ac2cd5bb41c59
PortaldeloteriasMacPoint <portaldeloteriasmacpoint@gmail.com>
2025-03-08 22:47:26 -0300

    Update mac.php

--- mac.php
+++ mac.php
@@ -1,6 +1,6 @@
 <?php
     // Exemplo de variável PHP que pode ser dinâmica
-    $title = "✨Portal de Loterias-Mac Point✨";
+    $title = "✨MAC POINT PREMIUM✨";
     $description = "Bem-vindo à plataforma Portal de Loterias- Mac Point . Aqui você encontra tudo sobre loterias!";
     $carousel_images = [
         "https://via.placeholder.com/600x150/FF5733",
